24dba90fbff2e721cfea4983457fd2ccedb30829
Joe Al Sokhen <jya20008@students.aust.edu.lb>
2024-05-15 17:58:13 +0300

    main: title change font weight. to w700
    Signed-off-by: Joe Al Sokhen <jya20008@students.aust.edu.lb>

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title, style: const TextStyle(fontWeight: FontWeight.w700)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
