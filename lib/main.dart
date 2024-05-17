deed0e3555d56e5a6f0b74145655d9b5fd36531d
Joe Al Sokhen <jya20008@students.aust.edu.lb>
2024-05-17 13:43:53 +0300

    main: Update regarding UI/UX and functionality
    Signed-off-by: Joe Al Sokhen <jya20008@students.aust.edu.lb>

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,18 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-
-import 'package:cryptography/cryptography.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DSA Hashing Project',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,16 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -59,108 +32,188 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _inputController = TextEditingController();
-  String? outputHashedValue;
+  List<int> hashArray = List.filled(10, 0);
+  bool isHashButtonEnabled = true;
+  String hashingProcess = '';
+
+  final ScrollController scrollController = ScrollController();
 
   void _calculateHash() {
     final input = _inputController.text.trim();
     if (input.isNotEmpty) {
-      int getHash = _calculateASCIIHash(input);
+      int hash = _calculateASCIIHash(input);
+      int index = hash % 10; // Determine the index using modulo
+      int newIndex = _findEmptySlot(index); // Find the nearest empty slot
       setState(() {
-        outputHashedValue = getHash.toString();
-      });
-    } else {
-      setState(() {
-        outputHashedValue = null;
+        hashArray[newIndex] = hash; // Store the hash value at the determined index
+        if (hashArray.every((element) => element != 0)) {
+          isHashButtonEnabled = false; // Disable hash button if array is full
+          _showArrayFullMessage();
+        }
       });
     }
   }
 
   int _calculateASCIIHash(String input) {
     int hashedValue = 0;
+    String process = '';
     for (int i = 0; i < input.length; i++) {
-      // input[i] to ascii conversion.
       int asciiValue = input.codeUnitAt(i);
-
-      // Hashing Function = 3k + 16, where k = asciiValue.
+      process += '${input[i]}: $asciiValue\n';
       hashedValue += (3 * asciiValue) + 16;
+      process += 'Hashing Step: (3 * $asciiValue) + 16 = ${(3 * asciiValue) + 16}\n\n';
     }
+    process += 'Total Hash Value: $hashedValue';
+    setState(() {
+      hashingProcess = process;
+    });
     return hashedValue;
+  }
+
+  int _findEmptySlot(int startIndex) {
+    int index = startIndex;
+    while (hashArray[index] != 0) {
+      index = (index + 1) % 10; // Move to the next index
+    }
+    return index;
+  }
+
+  void _clearInput() {
+    _inputController.clear();
+    setState(() {
+      hashArray = List.filled(10, 0); // Reset the hash array
+      isHashButtonEnabled = true; // Enable hash button
+      hashingProcess = ''; // Clear the hashing process
+    });
+  }
+
+  void _showArrayFullMessage() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Array is full'),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    double boxWidth = MediaQuery.of(context).size.width / 11;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title, style: const TextStyle(fontWeight: FontWeight.w700)),
+        title: Text(widget.title,
+            style: const TextStyle(fontWeight: FontWeight.w700)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              width: 400,
-              child: TextField(
-                controller: _inputController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter text to hash',
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _calculateHash,
-              child: const Text('Hash it'),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          // Calculate remaining height for the hashing process container
+          double inputHeight = 60; // Approximate height of input field
+          double buttonHeight = 50; // Approximate height of buttons
+          double paddingHeight = 16 + 16 + 20; // Total padding and margins
+          double availableHeight = constraints.maxHeight -
+              inputHeight -
+              buttonHeight -
+              paddingHeight;
+
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    if (outputHashedValue != null)
-                      const Text(
-                        'Hashed value:',
-                        style: TextStyle(fontSize: 18),
+                if (hashingProcess.isNotEmpty)
+                  SizedBox(
+                    height: availableHeight,
+                    width: double.infinity,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.purple[50],
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    if (outputHashedValue != null)
-                      const SizedBox(height: 8),
-                    if (outputHashedValue != null)
-                      SizedBox(
-                        width: 200,
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: Colors.purple[100],
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Center(
-                            child: Text(
-                              outputHashedValue!,
-                              style: const TextStyle(
-                                fontFamily: 'Courier',
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black54
-                              ),
+                      child: Scrollbar(
+                        thumbVisibility: true,
+                        controller: scrollController,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          controller: scrollController,
+                          child: Text(
+                            hashingProcess,
+                            style: const TextStyle(
+                              fontFamily: 'Courier',
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black54,
                             ),
                           ),
                         ),
                       ),
+                    ),
+                  ),
+                const SizedBox(height: 10),
+                Center(
+                  child: SizedBox(
+                    width: 400,
+                    child: TextField(
+                      controller: _inputController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter text to hash',
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: isHashButtonEnabled
+                          ? _calculateHash
+                          : null, // Disable hash button if array is full
+                      child: const Text('Hash it'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: _clearInput,
+                      child: const Text('Clear'),
+                    ),
                   ],
                 ),
               ],
             ),
-            ],
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.purple[100],
+        padding: const EdgeInsets.symmetric(vertical: 26, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            10,
+            (index) => Container(
+              width: boxWidth,
+              height: boxWidth * 1.1,
+              decoration: BoxDecoration(
+                color: Colors.purple[100],
+                border: Border.all(color: Colors.black26),
+                borderRadius: BorderRadius.circular(3),
+              ),
+              child: Center(
+                child: Text(
+                  '${hashArray[index]}',
+                  style: const TextStyle(
+                    fontFamily: 'Courier',
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
